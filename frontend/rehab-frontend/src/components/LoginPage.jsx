42f1c59c03afbcd7f6c60ae68de08c679ad73f9f
Oscar Ore <oscar.ore2011@gmail.com>
2024-04-12 22:49:31 -0400

    added login submut and clear button. Also set up basic validation for both email and apassword

--- frontend/rehab-frontend/src/components/LoginPage.jsx
+++ frontend/rehab-frontend/src/components/LoginPage.jsx
@@ -1,6 +1,20 @@
 import { Paper, TextField, Typography, Box, Button } from "@mui/material";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import { useState } from "react";
 
 export default function LoginPage() {
+	const [password, setPassword] = useState();
+	const [email, setEmail] = useState();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		if (!email || !password) {
+			alert("Please fill out all fields");
+			return;
+		}
+	};
+
 	return (
 		<Paper
 			sx={{
@@ -17,43 +31,61 @@ export default function LoginPage() {
 			>
 				Welcome to Rehab!
 			</Typography>
-			<TextField
-				placeholder='Enter Email'
-				type='email'
-				sx={{ width: "300px" }}
-			></TextField>
-			<br></br>
-			<br></br>
-			<TextField
-				placeholder='Enter Password'
-				type='password'
-				sx={{ width: "300px" }}
-			></TextField>
-			<Box
-				sx={{
-					display: "flex",
-					justifyContent: "center",
-				}}
-			>
-				<Button
-					sx={{
-						backgroundColor: "blue",
-						color: "white",
-						margin: 1
-					}}
-				>
-					Submit
-				</Button>
-				<Button
-					sx={{
-						backgroundColor: "blue",
-						color: "white",
-						margin: 1
-					}}
-				>
-					Clear
-				</Button>
+			<Box>
+				<AccountBoxIcon fontSize='large' />
 			</Box>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					placeholder='Enter Email'
+					type='email'
+					value={email}
+					required
+					onChange={(e) => setEmail(e.target.value)}
+					sx={{ width: "300px" }}
+				></TextField>
+				<br></br>
+				<br></br>
+				<TextField
+					placeholder='Enter Password'
+					type='password'
+					value={password}
+					required
+					onChange={(e) => setPassword(e.target.value)}
+					sx={{ width: "300px" }}
+				></TextField>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						margin: 1,
+					}}
+				>
+					<Button
+						sx={{
+							backgroundColor: "green",
+							color: "white",
+							margin: 1,
+						}}
+						type="submit"
+					>
+						Submit
+					</Button>
+					<Button
+						sx={{
+							backgroundColor: "red",
+							color: "white",
+							margin: 1,
+						}}
+						type='button'
+						onClick={() => {
+							setEmail("");
+							setPassword("");
+						}}
+					>
+						Clear
+					</Button>
+				</Box>
+			</form>
 		</Paper>
 	);
 }
